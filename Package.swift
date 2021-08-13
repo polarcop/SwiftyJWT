aac5f5d4582a5f3e2438c031958c844ba9b80c65
Rishi Serumadar <rishi8094@users.noreply.github.com>
2021-08-13 18:58:45 +0100

    Added SwiftyJWA as package

--- Package.swift
+++ Package.swift
@@ -20,11 +20,18 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "SwiftyJWT",
+            name: "SwiftyJWA",
             dependencies: [
                 .product(name: "CryptoSwift", package: "CryptoSwift")
             ],
-            exclude: ["HMAC/HMACCommonCrypto.swift"]),
+            exclude: [
+                "HMAC/HMACCommonCrypto.swift"
+            ]),
+        .target(
+            name: "SwiftyJWT",
+            dependencies: [
+                "SwiftyJWA"
+            ]),
         .testTarget(
             name: "SwiftyJWTTests",
             dependencies: ["SwiftyJWT"]),
