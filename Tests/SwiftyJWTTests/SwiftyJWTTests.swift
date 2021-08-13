00582b432eb98967a415d3ecc1df49d60c283bc5
Rishi Serumadar <rishi8094@users.noreply.github.com>
2021-08-13 19:02:01 +0100

    clean test

--- Tests/SwiftyJWTTests/SwiftyJWTTests.swift
+++ Tests/SwiftyJWTTests/SwiftyJWTTests.swift
@@ -1,11 +1,11 @@
-    import XCTest
-    @testable import SwiftyJWT
-
-    final class SwiftyJWTTests: XCTestCase {
-        func testExample() {
+import XCTest
+@testable import SwiftyJWT
+    
+final class SwiftyJWTTests: XCTestCase {
+    func testExample() {
             // This is an example of a functional test case.
             // Use XCTAssert and related functions to verify your tests produce the correct
             // results.
-//            XCTAssertEqual(SwiftyJWT().text, "Hello, World!")
-        }
+            //            XCTAssertEqual(SwiftyJWT().text, "Hello, World!")
     }
+}
