41071878a6b16557d59fc1ae9a25248fd8be3180
Rishi Serumadar <rishi8094@users.noreply.github.com>
2021-08-14 13:36:17 +0100

    Update IntegrationTests.swift

--- Tests/SwiftyJWTTests/IntegrationTests.swift
+++ Tests/SwiftyJWTTests/IntegrationTests.swift
@@ -4,20 +4,37 @@ import XCTest
 final class SwiftyJWTIntegrationTests: XCTestCase {
     func testVerificationFailureWithoutLeeway() {
         let token = SwiftyJWT.encode(.none) { builder in
-        builder.issuer = "fuller.li"
-        builder.audience = "cocoapods"
-        builder.expiration = Date().addingTimeInterval(-1) // Token expired one second ago
-        builder.notBefore = Date().addingTimeInterval(1) // Token starts being valid in one second
-        builder.issuedAt = Date().addingTimeInterval(1) // Token is issued one second in the future
-      }
-
-      do {
-        let _ = try SwiftyJWT.decode(token, algorithm: .none, leeway: 0)
-        XCTFail("InvalidToken error should have been thrown.")
-      } catch is SwiftyJWT.JWTErrors.InvalidToken {
-        // Correct error thrown
-      } catch {
-        XCTFail("Unexpected error type while verifying token.")
-      }
+            builder.issuer = "fuller.li"
+            builder.audience = "cocoapods"
+            builder.expiration = Date().addingTimeInterval(-1) // Token expired one second ago
+            builder.notBefore = Date().addingTimeInterval(1) // Token starts being valid in one second
+            builder.issuedAt = Date().addingTimeInterval(1) // Token is issued one second in the future
+        }
+        
+        do {
+            let _ = try SwiftyJWT.decode(token, algorithm: .none, leeway: 0)
+            XCTFail("InvalidToken error should have been thrown.")
+        } catch is SwiftyJWT.JWTErrors.InvalidToken {
+            // Correct error thrown
+        } catch {
+            XCTFail("Unexpected error type while verifying token.")
+        }
+    }
+    
+    func testVerificationSuccessWithLeeway() {
+        let token = SwiftyJWT.encode(.none) { builder in
+            builder.issuer = "fuller.li"
+            builder.audience = "cocoapods"
+            builder.expiration = Date().addingTimeInterval(-1) // Token expired one second ago
+            builder.notBefore = Date().addingTimeInterval(1) // Token starts being valid in one second
+            builder.issuedAt = Date().addingTimeInterval(1) // Token is issued one second in the future
+        }
+        
+        do {
+            let _ = try SwiftyJWT.decode(token, algorithm: .none, leeway: 2)
+            // Due to leeway no error gets thrown.
+        } catch {
+            XCTFail("Unexpected error type while verifying token.")
+        }
     }
 }
